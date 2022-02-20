87152e7745d5c7f257d87f0813ee7d808c7af8f1
Mohammad Mushtaq Ahmed <mushtaqahmed23599@gmail.com>
2022-02-20 13:48:01 +0530

    Add getYearlyExpense, getDailyExpense api's and update dbSchema

--- db/apis/budget.js
+++ db/apis/budget.js
@@ -1,11 +1,27 @@
 import * as db from "./util.js";
 
-export const getMonthlyExpenses = async (budgetId, month) => {
+export const getMonthlyExpense = async (budgetId, month) => {
   const monthlyExpense = await db.getDocument(
     `budgets/${budgetId}/monthly_expenses/${month}`
   );
   return monthlyExpense ? monthlyExpense : { id: month };
 };
 
-// const data = await getMonthlyExpenses("2xhwmTxgiCraAWKRPYos", "2-2022");
+export const getYearlyExpense = async (budgetId, year) => {
+  const yearlyExpense = await db.getDocument(
+    `budgets/${budgetId}/yearly_expenses/${year}`
+  );
+  return yearlyExpense ? yearlyExpense : { id: year };
+};
+
+export const getDailyExpense = async (budgetId, date) => {
+  const dailyExpense = await db.getDocument(
+    `budgets/${budgetId}/daily_expenses/${date}`
+  );
+  return dailyExpense ? dailyExpense : { id: date };
+};
+
+//const data = await getMonthlyExpense("2xhwmTxgiCraAWKRPYos", "2-2022");
+//const data = await getYearlyExpense("2xhwmTxgiCraAWKRPYos", "2022");
+// const data = await getDailyExpense("2xhwmTxgiCraAWKRPYos", "23-05-2022");
 // console.log(data);
