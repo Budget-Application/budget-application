f2954157533b883ad97074f92aff0b36186ceec0
Mohammad Mushtaq Ahmed <mushtaqahmed23599@gmail.com>
2022-02-25 21:22:38 +0530

    Update getUserBudgetList api and dbSchema

--- db/apis/user.js
+++ db/apis/user.js
@@ -1,9 +1,19 @@
 import * as db from "./util.js";
+import { DocumentReference, getDoc } from "firebase/firestore";
 
 export const getUserDetails = async (uid) => {
   return await db.getDocument(`users/${uid}`);
 };
 
 export const getUserBudgetList = async (uid) => {
-  return await db.getDocuments(`users/${uid}/budgets`);
+  const budgetRefs = await db.getDocuments(`users/${uid}/budgets`);
+  const budgets = [];
+  await Promise.all(budgetRefs.map(async (budgetRefs) => {
+    const docRef = budgetRefs["ref"];
+    if (docRef instanceof DocumentReference) {
+      const docSnap = await getDoc(docRef);
+      budgets.push({...docSnap.data(), id: docSnap.id});
+    }
+  }));
+  return budgets;
 };
