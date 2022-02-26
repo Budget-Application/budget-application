a4b308ce72e120c14eaf2d616e99b663c6fad23e
Mohammad Mushtaq Ahmed <mushtaqahmed23599@gmail.com>
2022-02-26 11:40:44 +0530

    Add doc to budget api's

--- db/apis/budget.js
+++ db/apis/budget.js
@@ -1,5 +1,10 @@
 import * as db from "./util.js";
 
+/**
+ * Function to get expense details from a budget for a given month
+ * @param {String} budgetId - The budgetId to get monthly expense
+ * @param {String} month - The month on which to get monthly expense e.g `05-1999`
+ */
 export const getMonthlyExpense = async (budgetId, month) => {
   const monthlyExpense = await db.getDocument(
     `budgets/${budgetId}/monthly_expenses/${month}`
@@ -7,6 +12,11 @@ export const getMonthlyExpense = async (budgetId, month) => {
   return monthlyExpense ? monthlyExpense : { id: month };
 };
 
+/**
+ * Function to get expense details from a budget for a given year
+ * @param {String} budgetId - The budgetId to get yearly expense
+ * @param {String} year - The year on which to get yearly expense e.g `1999`
+ */
 export const getYearlyExpense = async (budgetId, year) => {
   const yearlyExpense = await db.getDocument(
     `budgets/${budgetId}/yearly_expenses/${year}`
@@ -14,6 +24,11 @@ export const getYearlyExpense = async (budgetId, year) => {
   return yearlyExpense ? yearlyExpense : { id: year };
 };
 
+/**
+ * Function to get expense details from a budget for a given date
+ * @param {String} budgetId - The budgetId to get daily expense
+ * @param {String} date - The date on which to get daily expense e.g `23-05-1999`
+ */
 export const getDailyExpense = async (budgetId, date) => {
   const dailyExpense = await db.getDocument(
     `budgets/${budgetId}/daily_expenses/${date}`
@@ -21,7 +36,17 @@ export const getDailyExpense = async (budgetId, date) => {
   return dailyExpense ? dailyExpense : { id: date };
 };
 
-//const data = await getMonthlyExpense("2xhwmTxgiCraAWKRPYos", "2-2022");
-//const data = await getYearlyExpense("2xhwmTxgiCraAWKRPYos", "2022");
-// const data = await getDailyExpense("2xhwmTxgiCraAWKRPYos", "23-05-2022");
+/**
+ * Function to get expense names from a budget
+ * @param {String} budgetId - The budgetId to get expenseNames
+ */
+export const getExpenseNames = async (budgetId) => {
+  const budgetDetails = await db.getDocument(`budgets/${budgetId}`);
+  const expenseNames = budgetDetails["expense_names"];
+  return expenseNames;
+};
+
+// const data = await getDailyExpense("2xhwmTxgiCraAWKRPYos", "26-02-2022");
+// console.log(data);
+// const data = await getExpenseNames("2xhwmTxgiCraAWKRPYos");
 // console.log(data);
